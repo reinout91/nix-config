999d21a4d8929673daefe8a1ad71114abb579424
Reinout Holtrup <reinout91@gmail.com>
2025-12-10 22:33:35 +0100

    update lock

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -187,41 +187,4 @@ in
         userEmail = "reinout91@gmail.com";
       };
     };
-    xdg = {
-      mime.enable = true;
-      mimeApps = lib.mkIf (config.gnome.enable == false) {
-        enable = true;
-        defaultApplications = {
-          "image/jpeg" = [ "image-roll.desktop" "feh.desktop" ];
-          "image/png" = [ "image-roll.desktop" "feh.desktop" ];
-          "application/pdf" = [ "wps-office-pdf.desktop" "firefox.desktop" "google-chrome.desktop" ];
-          "application/zip" = "org.gnome.FileRoller.desktop";
-          "application/x-tar" = "org.gnome.FileRoller.desktop";
-          "application/x-bzip2" = "org.gnome.FileRoller.desktop";
-          "application/x-gzip" = "org.gnome.FileRoller.desktop";
-          "x-scheme-handler/http" = [ "firefox.desktop" "google-chrome.desktop" ];
-          "x-scheme-handler/https" = [ "firefox.desktop" "google-chrome.desktop" ];
-          "x-scheme-handler/about" = [ "firefox.desktop" "google-chrome.desktop" ];
-          "x-scheme-handler/unknown" = [ "firefox.desktop" "google-chrome.desktop" ];
-          "x-scheme-handler/mailto" = [ "gmail.desktop" ];
-          "audio/mp3" = "mpv.desktop";
-          "audio/x-matroska" = "mpv.desktop";
-          "video/webm" = "mpv.desktop";
-          "video/mp4" = "mpv.desktop";
-          "video/x-matroska" = "mpv.desktop";
-          "inode/directory" = "pcmanfm.desktop";
-        };
-      };
-      desktopEntries.image-roll = {
-        name = "image-roll";
-        exec = "${stable.image-roll}/bin/image-roll %F";
-        mimeType = [ "image/*" ];
-      };
-      desktopEntries.gmail = {
-        name = "Gmail";
-        exec = ''xdg-open "https://mail.google.com/mail/?view=cm&fs=1&to=%u"'';
-        mimeType = [ "x-scheme-handler/mailto" ];
-      };
-    };
-  };
 }
